c7a5c327aa7016bc9668b529aa9056dcc329454e
rothdavid <droth068@gmail.com>
2026-07-03 22:23:50 -0700

    getting data, qcew, for R

---
+++ code/R/qcew_data.R
@@ -0,0 +1,10 @@
+dir.create("data/raw", recursive = T, showWarnings = F)
+
+qcew_data <- function(YEAR, QUARTER){
+  
+  url <- https://data.bls.gov/cew/data/files/YEAR/csv/2023_qtrQUARTER_singlefile.zip
+  
+  download.file(download_qcewYEARQUARTER, destfile = "url", mode = "wb")
+  
+  print("Downloaded " download_qcewYEARQUARTER)
+}
