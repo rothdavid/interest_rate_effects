6ba693386b12333f5ad61022cbd754596362267b
rothdavid <droth068@gmail.com>
2026-07-04 16:35:57 -0700

    fix errors

--- code/R/qcew_data.R
+++ code/R/qcew_data.R
@@ -2,7 +2,7 @@ dir.create("data/raw", recursive = T, showWarnings = F)
 
 qcew_data <- function(YEAR, QUARTER){
   
-  url <- https://data.bls.gov/cew/data/files/YEAR/csv/2023_qtrQUARTER_singlefile.zip
+  url <- https://data.bls.gov/cew/data/files/YEAR/csv/YEAR_qtrQUARTER_singlefile.zip
   
   download.file(download_qcewYEARQUARTER, destfile = "url", mode = "wb")
   
