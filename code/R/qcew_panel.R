f29f7abd52eed184ccb87b1bb3a153853defe55f
rothdavid <droth068@gmail.com>
2026-07-19 18:22:35 -0700

    Built clean QCEW county panel data

--- code/R/qcew_panel.R
+++ code/R/qcew_panel.R
@@ -10,7 +10,7 @@ library("tidyverse")
 qcew_list <- list()
 
 for (x in 2018:2023) {
-  data <- glue::glue("summerEconProject26/interest_rate_effects/data/processed/qcew_county_total_{x}.csv")
+  data <- glue::glue("data/processed/qcew_county_total_{x}.csv")
   qcew_list[[as.character(x)]] <- read_csv(data)
   
 }
@@ -48,20 +48,24 @@ qcew_panel |>
 
 # load in State Data
 
-area_fips <- read_csv("summerEconProject26/interest_rate_effects/data/raw/area_fips.csv")                           
+area_fips <- read_csv("data/raw/area_fips.csv")                           
 
 area_fips <- area_fips |> 
   mutate(fip = str_sub(county_fipcode, 1, 2)) |> 
   select(state_name, fip) |> 
   distinct()
-                                                      
+
+# join state data
+                                                 
 qcew_panel1 <- qcew_panel |> 
   mutate(state_fips = str_sub(area_fips, 1, 2)) |> 
   left_join(area_fips, join_by(state_fips == fip)) |> 
-  mutate(state_name = case_when(area_fips == "72" ~ "Puerto Rico",
-                                area_fips == "78" ~ "U.S. Virgin Islands",
+  mutate(state_name = case_when(state_fips == "72" ~ "Puerto Rico",
+                                state_fips == "78" ~ "U.S. Virgin Islands",
                                 TRUE ~ state_name)) |> 
   select(-state_fips)
+
+# Zero and NA Investigation
 
 qcew_panel1 |> 
   filter(is.na(state_name)) |> 
@@ -77,14 +81,60 @@ qcew_panel1 |>
             zero_week_wage = sum(avg_wkly_wage == 0),
             na_week_wage = sum(is.na(avg_wkly_wage)))
 
+# Found 15 observations with 0's. 
+
 qcew_panel1 |> 
   filter(avg_emplvl == 0, avg_wkly_wage == 0, total_qtrly_wages == 0)
+
+# Created data time object and log vars
 
 qcew_panel1 <- qcew_panel1 |> 
   mutate(date = yq(glue::glue("{year}:{qtr}")),
          log_emplvl = case_when(avg_emplvl != 0 ~ log(avg_emplvl),
                                 TRUE ~ NA),
          log_wkly_wage = case_when(avg_wkly_wage != 0 ~ log(avg_wkly_wage),
-                                   TRUE ~ NA)
-         )
+                                   TRUE ~ NA)) |> 
+  arrange(area_fips, date) |>  
+  group_by(area_fips) |> 
+  mutate(wage_growth_yr = log_wkly_wage - lag(log_wkly_wage, 4),
+         wage_growth_qtr = log_wkly_wage - lag(log_wkly_wage, 1),
+         emplvl_growth_yr = log_emplvl - lag(log_emplvl, 4),
+         emplvl_growth_qtr = log_emplvl - lag(log_emplvl, 1))
 
+qcew_panel1 |> 
+  ggplot(mapping = aes(date, wage_growth_yr)) + 
+  geom_point() +
+  geom_line() +
+  facet_wrap(~state_name)
+
+qcew_panel1 |> 
+  ggplot(mapping = aes(date, emplvl_growth_yr)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~state_name)
+
+qcew_panel1 |>
+  ungroup() |> 
+  drop_na() |> 
+  summarise(a = max(wage_growth_yr),
+            b = max(emplvl_growth_yr),
+            c = min(wage_growth_yr),
+            d = min(emplvl_growth_yr))
+
+qcew_panel1 |> 
+  filter(emplvl_growth_yr >= 2)
+
+# removing counties that are undefined 
+
+qcew_panel_data <- qcew_panel1 |> 
+  filter(str_sub(area_fips, 3, 5) != "999")
+
+qcew_panel_data |> 
+  ungroup() |> 
+  drop_na() |> 
+  summarise(a = max(wage_growth_yr),
+            b = max(emplvl_growth_yr),
+            c = min(wage_growth_yr),
+            d = min(emplvl_growth_yr))
+
+write_csv(qcew_panel_data, "data/processed/qcew_panel_data.csv")
